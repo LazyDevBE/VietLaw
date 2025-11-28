3f0ae50c7481762da0bd22f7117281ac7eda6f37
LazyDevBE <hungtran11072005@gmail.com>
2025-11-28 11:52:20 +0700

    Update PhoneInput.jsx

--- frontend/src/components/PhoneInput.jsx
+++ frontend/src/components/PhoneInput.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { emailValidator } from '../utils/emailValidator'
+import { phoneValidator } from '../utils/phoneValidator'
 
 function PhoneInput({ 
   value, 
