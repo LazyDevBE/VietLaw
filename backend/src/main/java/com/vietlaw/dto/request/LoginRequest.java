0a6bca9cc159ea91a53a31eb3bdb67f447e304b3
LazyDevBE <hungtran11072005@gmail.com>
2025-11-28 12:32:03 +0700

    Update LoginRequest.java

--- backend/src/main/java/com/vietlaw/dto/request/LoginRequest.java
+++ backend/src/main/java/com/vietlaw/dto/request/LoginRequest.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
-@Data
 public class LoginRequest {
     @NotBlank(message = "Email không được để trống")
     @Email(message = "Email không hợp lệ")
@@ -12,4 +11,21 @@ public class LoginRequest {
 
     @NotBlank(message = "Password không được để trống")
     private String password;
+
+    // Getters and Setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
